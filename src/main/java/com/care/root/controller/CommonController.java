8dd1ed9f9a9f2896af4cf7825a15379695233ea7
unknown <solly0829@gmail.com>
2021-11-23 13:58:04 +0900

    include(header,footer), javascript에서 자동 문자열 옵션(inline)

--- src/main/java/com/care/root/controller/CommonController.java
+++ src/main/java/com/care/root/controller/CommonController.java
@@ -1,11 +1,17 @@
 package com.care.root.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.care.root.dto.TestDTO;
 
@@ -39,6 +45,16 @@ public class CommonController {
 		model.addAttribute("msg",message);
 		
 		return "object";
+	}
+	
+	@RequestMapping("operator/{num}")
+	public ModelAndView operator(@PathVariable int num, ModelAndView mav) {
+	   mav.setViewName("operator");
+	   List<String> arr = new ArrayList<String>();
+	   arr.add("홍길동"); arr.add("김개똥"); arr.add("고길동");
+	   mav.addObject("arr",arr); mav.addObject("num",num);
+	   mav.addObject("name","김말이");
+	   return mav;
 	}
 	
 	
