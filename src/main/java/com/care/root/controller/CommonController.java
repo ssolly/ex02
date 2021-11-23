f9a8e2aae2d4aa7570d5724c927d51b64e8a964a
unknown <solly0829@gmail.com>
2021-11-23 11:46:15 +0900

    컨트롤러의 session,model -> html thymeleaf 사용 예시

--- src/main/java/com/care/root/controller/CommonController.java
+++ src/main/java/com/care/root/controller/CommonController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.care.root.dto.TestDTO;
+
 @Controller
 public class CommonController {
 
@@ -17,5 +19,20 @@ public class CommonController {
 		session.setAttribute("user", "session value");
 		model.addAttribute("user","model value");
 		return "index";
+	}
+	
+	@GetMapping("object")
+	public String object(Model model) {
+		TestDTO dto01 = new TestDTO();
+		TestDTO dto02 = new TestDTO();
+		dto01.setName("홍길동-01");
+		dto01.setAddr("산골짜기-01");
+		dto02.setName("홍길동-02");
+		dto02.setAddr("산골짜기-02");
+		
+		session.setAttribute("user", dto01);
+		model.addAttribute("user",dto02);
+		
+		return "object";
 	}
 }
